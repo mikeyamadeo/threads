43bf61ce1d95a16b593f64cf7232df50b72a1ce9
Mikey Murphy <mikeyamadeo@gmail.com>
2017-08-30 21:44:14 -0600

    extract styles from navigation

--- src/App/index.js
+++ src/App/index.js
@@ -1,45 +1,28 @@
 import './style'
 import React from 'react'
-import { Route, Redirect, Link, withRouter } from 'react-router-dom'
-import { Nav, Div, Ul, Li } from 'glamorous'
+import { Route, Redirect, Link } from 'react-router-dom'
 import { Shop, About, Cart } from './views'
 
-const NavHeight = '50px'
-const Navigation = withRouter(({ location }) => (
-  <Div
-    position='fixed'
-    top='0'
-    left='0'
-    width='100%'
-    backgroundColor='rgba(255, 255, 255, 0.95)'
-    height={NavHeight}
-    display='flex'
-    alignItems='center'
-  >
-    <Nav
-      display='flex'
-      width='100%'
-      maxWidth='800px'
-      margin='0 auto'
-      justifyContent='space-between'
-    >
-      <Ul display='flex' listStyle='none'>
-        <Li><Link to='/about'>About</Link></Li>
-        <Li><Link to='/shop'>Shop</Link></Li>
-      </Ul>
-      <Link to='/cart'>Cart</Link>
-    </Nav>
-  </Div>
-))
+const Navigation = ({ location }) => (
+  <div>
+    <nav>
+      <ul>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/shop'>Shop</Link></li>
+        <li><Link to='/cart'>Cart</Link></li>
+      </ul>
+    </nav>
+  </div>
+)
 
 const App = () => (
-  <Div paddingTop={NavHeight}>
+  <div>
     <Navigation />
     <Redirect from='/' to='/about' />
     <Route path='/about' component={About} />
     <Route path='/shop' component={Shop} />
     <Route path='/cart' component={Cart} />
-  </Div>
+  </div>
 )
 
 export default App
