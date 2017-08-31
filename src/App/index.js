0bc7f80e7f5bf0cfca8c0e11ddcaf662bac195ee
Mikey Murphy <mikeyamadeo@gmail.com>
2017-08-31 12:20:56 -0600

    simplify more by removing "About" section

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,12 @@
 import './style'
 import React from 'react'
 import { Route, Redirect, Link } from 'react-router-dom'
-import { Shop, About, Checkout } from './views'
+import { Shop, Checkout } from './views'
 
 const Navigation = () => (
   <div>
     <nav>
       <ul>
-        <li><Link to='/about'>About</Link></li>
         <li><Link to='/shop'>Shop</Link></li>
         <li><Link to='/checkout'>Checkout</Link></li>
       </ul>
@@ -18,8 +17,7 @@ const Navigation = () => (
 const App = () => (
   <div>
     <Navigation />
-    <Redirect from='/' to='/about' />
-    <Route path='/about' component={About} />
+    <Redirect from='/' to='/shop' />
     <Route path='/shop' component={Shop} />
     <Route path='/checkout' component={Checkout} />
   </div>
--- src/App/views/index.js
+++ src/App/views/index.js
@@ -1,3 +1,2 @@
-export const About = require('./About').default
 export const Checkout = require('./Checkout').default
 export const Shop = require('./Shop').default
